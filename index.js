4d469a180b659955e9c846a787c1f6a3df00b7a5
suraj82 <suraj82@bitbucket.org>
2020-06-09 16:07:21 +0530

    comment mongo

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const config = require('./config/keys');
 require('./models/Registration');
 require('./models/Demand');
 require('./models/Cupon');
-mongoose.connect(config.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
+//mongoose.connect(config.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 //mongoose.connect('mongodb+srv://root:root@cluster0-2cesk.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
 
@@ -17,6 +17,6 @@ app.use(bodyParser.json());
 require('./routes/dialogFlowRoutes')(app);
 require('./routes/fulfillmentRoutes')(app);
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT);
 
